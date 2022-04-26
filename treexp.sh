34745f148cccd37383a481e69b92c76ee5c84c74
Tommy Anfosso <anfossotommy@hotmail.fr>
2022-04-27 00:09:11 +0200

    refactor

--- treexp.sh
+++ treexp.sh
@@ -8,9 +8,10 @@ res=
 
 for f in "$dir"*; do
     nb_of_lines=$(wc -l < "$f")
-    res="$res./${f#"$dir"} $((nb_of_lines = nb_of_lines + 1))\n$(cat "$f")\n"
+    res="$res./${f#"$dir"} $((nb_of_lines + 1))\n$(cat "$f")\n"
+    # if file ends with trailing newline, add it before returning to line
     [ -z "$(tail -c1 "$f")" ] && res="$res\n"
 done
 
 # we get rid of the last '\n' written in $res
-echo "$res" | head -n-1 > "${dir%/}".txt
+echo "$res\c" > "${dir%/}".txt
